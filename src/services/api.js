4ba5bbe6d995f235e8e6ba50b15d9b8eb6c76501
Migui12 <134223160+Migui12@users.noreply.github.com>
2026-02-19 14:09:01 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://ugel-api.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://ugel-api.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
